5a46f60a038a2bc15e5ca3e5df18dd321136981a
KimKyubea <skdhdak516@gmail.com>
2026-03-18 11:51:07 +0900

    fix: ultralytic training save path setting

--- core/trainer/trainer.py
+++ core/trainer/trainer.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 
-from ultralytics import YOLO
+import os
+from ultralytics import YOLO, settings
+settings.update({"mlflow": False})
 
 class UltralyticsTrainer():
     def __init__(self, config, logger=None):
@@ -8,21 +10,33 @@ class UltralyticsTrainer():
         self.model = YOLO(self.config["model"])
 
     def train(self):
-        self.model.train(
-            data    = self.config["dataset"], # Yolo yaml file path
-            epochs  = self.config.get("epochs", 50),
-            imgsz   = self.config.get("imgsz", 640),
-            batch   = self.config.get("batch", 4),
-            device  = self.config.get("device", 0),
-            project = self.config.get("project", "proejct_name"),
-            workers = self.config.get("workers", 4),
-            cache   = self.config.get("cache", False),
+        prj_dir = self.config.get("project_dir", "runs")
+        prj_name = self.config.get("project_name", "train")
+        prj_pull = os.path.join(prj_dir, prj_name)
 
-            freeze  = self.config.get("freeze", 5)
-        )
+        train_args = {
+            "data": self.config["dataset"],
+            "epochs": self.config.get("epochs", 50),
+            "imgsz": self.config.get("imgsz", 640),
+            "batch": self.config.get("batch", 4),
+            "device": self.config.get("device", 0),
+            "workers": self.config.get("workers", 4),
+            "cache": self.config.get("cache", False),
+            "freeze": self.config.get("freeze", 0),
 
-        # TODO # 
-        # logging #
+            "save_dir": prj_pull
+        }
+
+        extra_args = self.config.get("yolo_args", {})
+
+        for key in extra_args:
+            train_args[key] = extra_args[key]
+
+        print("[INFO] train args ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ")
+        for k, v in train_args.items():
+            print(f"{k}: {v}")
+
+        self.model.train(**train_args)
 
 class RFDETRTrainer():
     def __init__(self, config, logger=None):
