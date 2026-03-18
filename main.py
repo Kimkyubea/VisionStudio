16d1c048f8ddb28745b289087a2a0976abe340e9
KimKyubea <skdhdak516@gmail.com>
2026-03-18 11:51:51 +0900

    feat: add mlflow logging, evaluation result and train configurations

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 
 import os, sys
 
-from vs_cli.train import run_train
-from vs_cli.visualize import run_visualize
-from vs_cli.evaluate import run_evaluate
 from utils.configuration_manager import load_config
 
 def main():
@@ -19,16 +16,29 @@ def main():
     print("[INFO]: Configuration loading DONE")
 
     if cmd == "train":
+        from vs_cli.train import run_train
         run_train(cfg)
         return
 
     elif cmd == "visualize":
+        from vs_cli.visualize import run_visualize
         run_visualize(cfg)
         return
 
     elif cmd == "evaluate":
+        from vs_cli.evaluate import run_evaluate
         run_evaluate(cfg)
         return
+
+    elif cmd == "log_eval":
+        from vs_cli.log_mlflow import log_evaluation
+        log_evaluation(cfg)
+
+    # elif cmd == "log_release_note":
+    #     from vs_cli.log_mlflow import log_release_note
+
+    # elif cmd == "upload_modle":
+    #     from vs_cli.log_mlflow import upload_model
 
 if __name__ == "__main__":
     main()
